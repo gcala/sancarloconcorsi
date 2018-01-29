f11768645893329c13dbf5f11b6edfc50dc09a30
Giuseppe Calà <jiveaxe@gmail.com>
2018-01-29 17:35:54 +0100

    aggiunta pagina template iniziale

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, send_file, request, redirect, url_for, send_from_directory
+#from flask_uploads import UploadSet, configure_uploads, DOCUMENTS
+from werkzeug import secure_filename
 app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def main():
-    return "Welcome Concorsi San Carlo!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
